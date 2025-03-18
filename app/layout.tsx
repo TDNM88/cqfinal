67c5056e44e187e2620b46a0a0e6959e310f4305
TDNM88 <167914317+TDNM88@users.noreply.github.com>
2025-03-18 13:58:08 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'CaslaQuartz AI',
+  description: 'Developed by TDNM',
+  generator: 'Flux',
 }
 
 export default function RootLayout({
