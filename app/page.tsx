f315e33c3db5c3464ca02814ae0ce6df3df182f2
TDNM88 <167914317+TDNM88@users.noreply.github.com>
2025-03-23 05:40:21 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -785,8 +785,9 @@ export default function ImageInpaintingApp() {
       </div>
 
       <footer className="mt-12 py-4 text-center text-sm text-blue-900/70">
-        <p>Liên hệ: support@caslaquartz.com | Hotline: 1234-567-890</p>
+        <p>Liên hệ: info@caslaquartz.com | Hotline: +84 971 52 53 54 </p>
         <p>© 2025 CaslaQuartz. All rights reserved.</p>
+        <p> Ứng dụng và Mô hình AI được phát triển bởi TDNM.</p>
       </footer>
     </div>
   );
